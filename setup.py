fa1dadbe286d724725e1dbba0ee37157195beed2
Anthony Sottile <asottile@umich.edu>
2017-02-25 15:14:53 -0800

    v0.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='aspy.refactor_imports',
     description="Utilities for refactoring imports in python-like syntax.",
     url='https://github.com/asottile/aspy.refactor_imports',
-    version='0.5.1',
+    version='0.5.2',
 
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
