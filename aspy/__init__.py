cc6467d1c4b90508aecb6fd6725336e69b8070cc
Anthony Sottile <asottile@umich.edu>
2021-12-20 17:03:39 -0500

    remove unused type ignore

--- aspy/__init__.py
+++ aspy/__init__.py
@@ -1,2 +1,2 @@
 # This is a namespace package
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)  # type: ignore  # noqa: E501
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
